4bcdb3fe392b50c16b4aae96eb58f4f2d13b12cd
unknown <hayatfaisal586@gmail.com>
2023-05-12 17:14:50 +0500

    updated

--- src/app/all-purchase/all-purchase.component.ts
+++ src/app/all-purchase/all-purchase.component.ts
@@ -15,7 +15,7 @@ export interface PurchaseData {
   id: number;
   invoice: number;
   title: string;
-  supplier_amount: string;
+  invoiceNumber: string;
   warehouse: string;
   product: string;
   quantity: number;
@@ -29,7 +29,7 @@ interface Product {
   id: number;
   name: string;
   price: number;
-  product:string;
+  product: string;
 }
 
 // Define a row interface to represent a row in the table
@@ -38,7 +38,6 @@ interface Row {
   price: number;
   quantity: number;
   total: number;
-  
 }
 @Component({
   selector: 'app-all-purchase',
@@ -49,6 +48,7 @@ export class AllPurchaseComponent implements OnInit {
   public isProductSelected: boolean = false;
   // public selectedProduct: any;
   selectedProducts: {
+    id: any;
     product: string;
     quantity: number;
     price: number;
@@ -59,58 +59,82 @@ export class AllPurchaseComponent implements OnInit {
   currentPage = 1;
   totalPages!: number;
   pages: number[] = [];
-  id = 'pagination';
+  id: any;
   closeResult: any;
   AllPurchaseData: any[] = [];
   totalItems: any;
   itemsPerPage: any;
   products: any[] = [];
   suppliers: any[] = [];
-  total:number=0;
+  total: number = 0;
   productData: any[] = [];
-  purchaseData: PurchaseData = {
-    id: 0,
-    invoice: 0,
-    title: '',
-    name: '',
-    supplier_amount: '',
-    warehouse: '',
-    total: 0,
-    product: '',
-    quantity: 0,
-    amount: 0,
-    voucher_type: '',
-    date: new Date(),
-  };
-  
+  purchaseInvoice?: number;
+  purchaseDate?: Date;
+  selectedSupplier: any;
+  selectedWarehouse: any;
+  payableAmount?: number;
+  selectedRemark: any;
+
+  // purchaseData: PurchaseData = {
+  //   id: 0,
+  //   invoice: 0,
+  //   title: '',
+  //   name: '',
+  //   supplier_amount: '',
+  //   warehouse: '',
+  //   total: 0,
+  //   product: '',
+  //   quantity: 0,
+  //   amount: 0,
+  //   voucher_type: '',
+  //   date: new Date(),
+  // };
 
   onPageChange(event: any) {
     this.currentPage = event;
   }
+
   quantity: number = 0;
   price: number = 0;
   Alltotal: number = 0;
   total1: number = 0;
   total2: number = 0;
   discount: number = 0;
-grandTotal: number = 0;
-updateTotal() {
-  let total = 0;
-  for (let row of this.rows) {
-    total += row.price * row.quantity;
+  grandTotal: number = 0;
+  totalQuantity: number = 0;
+
+  // updateTotal() {
+  //   let total = 0;
+  //   let quantityTotal = 0;
+  //   for (let row of this.rows) {
+  //     total += row.price * row.quantity;
+  //     quantityTotal += row.quantity;
+  //   }
+  //   this.grandTotal = total - this.discount;
+  //   this.totalQuantity = quantityTotal;
+  // }
+  updateTotal() {
+    let total = 0;
+    let quantityTotal = 0;
+    for (let row of this.rows) {
+      total += row.price * row.quantity;
+      quantityTotal += row.quantity;
+    }
+    this.grandTotal = total - this.discount;
+    this.totalQuantity = quantityTotal;
   }
-  this.grandTotal = total - this.discount;;
-}
 
-// modify the updateTotal() function
-// updateTotal() {
-//   this.rows.forEach(row => {
-//     row.total = row.price * row.quantity;
-//   });
+  // quantity += row.quantity;
+  // let quantity = 0;
 
-//   this.grandTotal = this.rows.reduce((acc, row) => acc + row.total, 0);
-// }
+  // modify the updateTotal() function
+  // updateTotal() {
+  //   this.rows.forEach(row => {
+  //     row.total = row.price * row.quantity;
+  //   });
 
+  //   this.grandTotal = this.rows.reduce((acc, row) => acc + row.total, 0);
+  // }
 
   // updateTotal() {
   //   this.total = this.quantity * this.price - this.discount;
@@ -161,7 +185,8 @@ updateTotal() {
       this.productData = <any>Response.results;
     });
   }
-  selectedProduct = <any> {
+  selectedProduct = <any>{
+    id: '',
     name: '',
     quantity: 0,
     price: 0,
@@ -169,8 +194,6 @@ updateTotal() {
   };
 
   productList: any[] = [];
-
-
 
   newProduct = {
     name: this.selectedProduct.name.toString(),
@@ -180,52 +203,234 @@ updateTotal() {
     total: this.total,
   };
 
-
   // Define the addProduct method to add the selected product to the table
-addProduct() {
-  // Ensure a product is selected
-  if (!this.selectedProduct) {
-    return;
+  addProduct() {
+    // Ensure a product is selected
+    if (!this.selectedProduct) {
+      return;
+    }
+
+    // Find the selected product in the productData array
+    const selectedProduct = this.productData.find(
+      (p) => p.id === this.selectedProduct
+    );
+
+    // Calculate the total price based on the product price and quantity
+    const total = selectedProduct.price * this.quantity;
+
+    // Create a new row object with the selected product and input values
+    const newRow: Row = {
+      product: selectedProduct,
+      price: selectedProduct.price,
+      quantity: this.quantity,
+      total: total,
+    };
+
+    // Add the new row to the array of rows
+    this.rows.push(newRow);
+
+    // Clear the input fields
+    this.selectedProduct = '';
   }
 
-  // Find the selected product in the productData array
-  const selectedProduct = this.productData.find(p => p.id === this.selectedProduct);
+  get formattedData(): string {
+    // Map the rows array to an array of strings representing each row
+    const rowStrings = this.rows.map(
+      (row) => `${row.product.name}: $${row.total.toFixed(2)}`
+    );
 
-  // Calculate the total price based on the product price and quantity
-  const total = selectedProduct.price * this.quantity;
- 
+    // Join the row strings with line breaks
+    return rowStrings.join('\n');
+  }
+  removeProduct(index: number) {
+    this.rows.splice(index, 1);
+    this.updateTotal();
+  }
+  i: any;
 
-  // Create a new row object with the selected product and input values
-  const newRow: Row = {
-    product: selectedProduct,
-    price: selectedProduct.price,
-    quantity: this.quantity,
-    total: total,
-   
-  };
+  // addPurchase() {
+  //   const payload:any = {
+  //     invoice_no: this.purchaseInvoice,
+  //     date: this.purchaseDate,
+  //     account: this.selectedSupplier,
+  //     warehouse: this.selectedWarehouse,
+  //     amount: this.grandTotal,
+  //     quantity: this.totalQuantity,
+  //     title: this.selectedRemark,
+  //   };
+  //   console.log(payload);
+  //   let purchaseId: number;
+  //   this.http.post("http://192.168.1.9:8000/inventory/stocks_purchase/", payload).subscribe((response) => {
+  //     purchaseId=response.id;
+  //     console.log(purchaseId);
+  //   });
 
-  // Add the new row to the array of rows
-  this.rows.push(newRow);
+  // }
+  addPurchase() {
+    const payload: any = {
+      invoice_no: this.purchaseInvoice,
+      date: this.purchaseDate,
+      account: this.selectedSupplier,
+      warehouse: this.selectedWarehouse,
+      amount: this.grandTotal,
+      quantity: this.totalQuantity,
+      title: this.selectedRemark,
+    };
+    console.log(payload);
 
-  // Clear the input fields
-  this.selectedProduct=''
+    this.http
+      .post<{ id: number }>(
+        'http://192.168.1.9:8000/inventory/stocks_purchase/',
+        payload
+      )
+      .subscribe((response) => {
+        const purchaseId = response.id;
+        console.log(purchaseId);
+        this.addStock(purchaseId);
+      });
+  }
+
+  // addStock(id: any) {
+  //   let products = [];
+  //   for (let row of this.rows) {
+  //     let product = {
+  //       product: row.product.id,
+  //       quantity: row.quantity,
+  //       amount: row.quantity * row.price,
+  //     };
+  //     products.push(product);
+  //   }
+
+  //   const payload = {
+  //     date: this.purchaseDate,
+  //     account_supplier: this.selectedSupplier,
+  //     warehouse: this.selectedWarehouse,
+  //     title: this.selectedRemark,
+  //     products: products,
+  //     purchase_id: id,
+  //   };
+
+  //   console.log(payload);
+  //   this.http
+  //     .post(
+  //       `http://localhost:3000/inventory/stocks_purchase/${id}/stocks`,
+  //       payload
+  //     )
+  //     .subscribe((response) => {
+  //       console.log(response);
+  //     });
+  // }
+  // addStock(id: any) {
+  //   if (id === undefined) {
+  //     console.error('No purchase ID specified');
+  //     return;
+  //   }
   
+ 
+  // let newProducts:any[]=[];
+
+  //   for (let row of newProducts) {
+  //     let product = {
+  //       product: row.product.id,
+  //       quantity: row.quantity,
+  //       amount: row.quantity * row.price,
+  //     };
+  //     newProducts.push(product);
+  //   }
+  //   const payload = {
+  //     date: this.purchaseDate,
+  //     account_supplier: this.selectedSupplier,
+  //     warehouse: this.selectedWarehouse,
+  //     title: this.selectedRemark,
+  //     products: this.newProduct.map((row) => {
+  //             return {
+  //               product: row.product.id,
+  //               quantity: row.quantity,
+  //               amount: row.quantity * row.price,
+  //             };
+  //           }),
+  //     quantity:this.totalQuantity,
+  //     purchase_id: id,
+  //   };
+  
+  
+  //   console.log(payload);
+  //   this.http
+  //     .post(
+  //       `http://192.168.1.9:8000/inventory/stocks_purchase/${id}/stocks/`,
+  //       payload
+  //     )
+  //     .subscribe((response) => {
+  //       const purchaseId = response;
+  //       console.log(purchaseId);
+  //     });
+  // }
+  
+
+//   addStock(id:any) {
+//     let Products:any[]=[];
+
+//     for (let item of Products) {
+//       let product = {
+//         product: item.product.id,
+//         quantity: item.quantity,
+//         amount: item.quantity * item.price,
+//       };
+//       Products.push(product);
+//     }
+//     const formData = {
+//       date: this.purchaseDate,
+//       account_supplier: this.selectedSupplier,
+//       warehouse: this.selectedWarehouse,
+//       title: this.selectedRemark,
+//       quantity:this.totalQuantity,
+//       purchase_id: id,
+//       voucher_type:'purchase',
+    
+//     };
+//     console.log(formData);
+//   this.http.post(`http://192.168.1.9:8000/inventory/stocks_purchase/${id}/stocks/`, formData).subscribe((response) => {
+//     console.log(response);
+//   });
+ 
+// }
+addStock(id: any) {
+  let Products = [];
+  for (let row of this.rows) {
+    let product = {
+      product: row.product.id,
+      quantity: row.quantity,
+      amount: row.quantity * row.price,
+        date: this.purchaseDate,
+        account_supplier: this.selectedSupplier,
+        warehouse: this.selectedWarehouse,
+        title: this.selectedRemark,
+        purchase_id: id,
+        vocuher_type:'Purchase',
+         // include the products array in the payload
+      };
+      
+    
+      console.log(product);
+    
+      this.http.post(`http://192.168.1.9:8000/inventory/stocks_purchase/${id}/stocks/`, product).subscribe((response) => {
+        console.log(response);
+      });
+    };
+    
+  }
+  
+
+  // const formData = {
+  //   date: this.purchaseDate,
+  //   account_supplier: this.selectedSupplier,
+  //   warehouse: this.selectedWarehouse,
+  //   title: this.selectedRemark,
+  //   products: products,
+  //   quantity:this.totalQuantity,
+  //   purchase_id: id,
+  //   voucher_type: "purchase",
+  // };
+ 
 }
 
-get formattedData(): string {
-  // Map the rows array to an array of strings representing each row
-  const rowStrings = this.rows.map(row => `${row.product.name}: $${row.total.toFixed(2)}`);
-
-  // Join the row strings with line breaks
-  return rowStrings.join('\n');
-}
-removeProduct(index: number) {
-  this.rows.splice(index, 1);
-  this.updateTotal();
-}
-i:any;
-
-addPurchase(){
-
-}
-}
